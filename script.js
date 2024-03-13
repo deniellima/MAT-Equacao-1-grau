2c64377c6074f040a15ab054c4a4b0ad93370df0
Deniel <97967222+deniellima@users.noreply.github.com>
2024-03-13 16:19:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -77,6 +77,67 @@ const quizArray = [
     options: ["-24", "12", "6", "24"],
     correct: "6",
   },
+
+  {
+    id: "10",
+    question: "x + 4 = 10",
+    options: ["13", "-25", "6", "9"],
+    correct: "6",
+  },
+  {
+    id: "11",
+    question: "x + 101 = 300",
+    options: ["199", "-251", "639", "139"],
+    correct: "199",
+  },
+  {
+    id: "12",
+    question: "x - 279 = 237",
+    options: ["56", "499", "-518", "516"],
+    correct: "516",
+  },
+  {
+    id: "13",
+    question: "x - 8 = -10",
+    options: ["24", "5", "-2", "9"],
+    correct: "-2",
+  },
+  {
+    id: "14",
+    question: "x + 9 = -1",
+    options: ["-10", "10", "29", "32"],
+    correct: "-10",
+  },
+  {
+    id: "15",
+    question: "3x = 12",
+    options: ["15", "9", "-5", "4"],
+    correct: "4",
+  },
+  {
+    id: "16",
+    question: "9x = 18",
+    options: ["26", "2", "-4", "21"],
+    correct: "2",
+  },
+  {
+    id: "17",
+    question: "35x = -105",
+    options: ["-3", "12", "29", "16"],
+    correct: "-3",
+  },
+  {
+    id: "18",
+    question: "7x - 1 = 13",
+    options: ["2", "50", "-4", "64"],
+    correct: "2",
+  },
+  {
+    id: "19",
+    question: "6x - 10 = 2x + 14",
+    options: ["-24", "12", "6", "24"],
+    correct: "6",
+  },
 ];
 
 //Restart Quiz
@@ -93,17 +154,18 @@ nextBtn.addEventListener(
     //increment questionCount
     questionCount += 1;
     //if last question
-    if (questionCount == quizArray.length) {
+    if (questionCount == 10) {
       //hide question container and display score
       displayContainer.classList.add("hide");
       scoreContainer.classList.remove("hide");
       //user score
       userScore.innerHTML =
-        "Your score is " + scoreCount + " out of " + questionCount;
+        "Sua pontuação é " + scoreCount + " de " + questionCount;
+        confetti();
     } else {
       //display questionCount
       countOfQuestion.innerHTML =
-        questionCount + 1 + " of " + quizArray.length + " Question";
+        questionCount + 1 + " de 10 questões";
       //display quiz
       quizDisplay(questionCount);
       count = 31;
@@ -117,7 +179,7 @@ nextBtn.addEventListener(
 const timerDisplay = () => {
   countdown = setInterval(() => {
     count--;
-    timeLeft.innerHTML = `${count}s`;
+    timeLeft.innerHTML = count + "s";
     if (count == 0) {
       clearInterval(countdown);
       displayNext();
@@ -140,30 +202,40 @@ const quizDisplay = (questionCount) => {
 function quizCreator() {
   //randomly sort questions
   quizArray.sort(() => Math.random() - 0.5);
-  //generate quiz
-  for (let i of quizArray) {
+
+  // Generate 10 random questions instead of all
+  const numberOfQuestions = 10;
+  for (let i = 0; i < numberOfQuestions; i++) {
+    let currentQuestion = quizArray[i];
+
     //randomly sort options
-    i.options.sort(() => Math.random() - 0.5);
+    currentQuestion.options.sort(() => Math.random() - 0.5);
+
     //quiz card creation
     let div = document.createElement("div");
     div.classList.add("container-mid", "hide");
+
     //question number
-    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    countOfQuestion.innerHTML = "1 de 10 questões";
+
     //question
     let question_DIV = document.createElement("p");
     question_DIV.classList.add("question");
-    question_DIV.innerHTML = i.question;
+    question_DIV.innerHTML = currentQuestion.question;
     div.appendChild(question_DIV);
+
     //options
     div.innerHTML += `
-    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
-     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
-      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
-       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+      <button class="option-div" onclick="checker(this)">${currentQuestion.options[0]}</button>
+      <button class="option-div" onclick="checker(this)">${currentQuestion.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${currentQuestion.options[2]}</button>
+      <button class="option-div" onclick="checker(this)">${currentQuestion.options[3]}</button>
     `;
+
     quizContainer.appendChild(div);
   }
 }
+
 
 //Checker Function to check if option is correct or not
 function checker(userOption) {
